a1d6c4d03a17acb70735845e58a8b90748cb4a18
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-05-12 15:33:51 +0300

    Validate email and DOM

--- src/Component/Input.jsx
+++ src/Component/Input.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 function Input(){
+    //use state hook to store value entered in input
+    const [inputVal, setInputVal] = useState("");
+
+    function handleInput(event){
+        const value = event.target.value;
+        setInputVal(value);
+    }
+    //Form validation with DOM
+    function handleClick(e){
+        var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        if(inputVal.match(validRegex)){
+            document.getElementById("inputField").style.border="2px solid #ce9797";
+            document.getElementById("inputField").style.background="none";
+            document.getElementById("error").style.visibility = "hidden";
+
+        }else{
+            document.getElementById("inputField").style.border="2px solid red";
+            document.getElementById("inputField").style.background ="url(/images/icon-error.svg) no-repeat 13rem";
+            document.getElementById("inputField").style.opacity = "1";
+            document.getElementById("error").style.visibility = "visible";
+        }
+
+        e.preventDefault();
+    }
+
     return <div className="input-field">
-        <input type="email" placeholder="Email Address"/>
-        <button/>
+        <input id="inputField" onChange={handleInput} type="email" placeholder="Email Address" value={inputVal}/>
+        <button onClick={handleClick}/>
+        <p id="error">Please provide a valid email</p>
     </div>
 }
 
